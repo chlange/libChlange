efbd0ce69a7a6d2b68c6785729ff1c5237fbe1c9
Christian Lange <chlange@htwg-konstanz.de>
2013-07-30 12:21:24 +0200

    Added minimum requirements for type float

---
+++ include/float/float.h
@@ -0,0 +1,21 @@
+#ifndef FLOAT_H
+#define FLOAT_H
+
+/* 
+ * The following describes an artificial floating-point representation 
+ * that meets the minimum requirements of the Standard, and the 
+ * appropriate values in a <float.h> header for type float
+*/
+
+#define FLT_RADIX                       16
+#define FLT_MANT_DIG                     6
+#define FLT_EPSILON        9.53674316E-07F
+#define FLT_DIG                          6
+#define FLT_MIN_EXP                    -31
+#define FLT_MIN            2.93873588E-39F
+#define FLT_MIN_10_EXP                 -38
+#define FLT_MAX_EXP                    +32
+#define FLT_MAX            3.40282347E+38F
+#define FLT_MAX_10_EXP                 +38
+
+#endif
--- include/locale/locale.h
+++ include/locale/locale.h
@@ -1,6 +1,27 @@
 #ifndef LOCALE_H
 #define LOCALE_H
 
+struct lconv {
 
+         char *decimal_point;       /* "." */
+         char *thousands_sep;       /* "" */
+         char *grouping;            /* "" */
+         char *int_curr_symbol;     /* "" */
+         char *currency_symbol;     /* "" */
+         char *mon_decimal_point;   /* "" */
+         char *mon_thousands_sep;   /* "" */
+         char *mon_grouping;        /* "" */
+         char *positive_sign;       /* "" */
+         char *negative_sign;       /* "" */
+         char int_frac_digits;      /* CHAR_MAX */
+         char frac_digits;          /* CHAR_MAX */
+         char p_cs_precedes;        /* CHAR_MAX */
+         char p_sep_by_space;       /* CHAR_MAX */
+         char n_cs_precedes;        /* CHAR_MAX */
+         char n_sep_by_space;       /* CHAR_MAX */
+         char p_sign_posn;          /* CHAR_MAX */
+         char n_sign_posn;          /* CHAR_MAX */
+
+};
 
 #endif
